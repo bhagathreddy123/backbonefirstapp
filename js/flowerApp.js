cef415568b84a3a46828617dab358d632b086318
bhagath <bhagath@getcustomexposure.com>
2017-05-31 15:16:47 +0530

    adding instances for a model changed instance property example

--- js/flowerApp.js
+++ js/flowerApp.js
@@ -1,35 +1,56 @@
 //creting 3 instances of singleFlower
-var redRoses = new app.singleFlower({
-	name: "RedRoses",
-	price: 39.05,
-	color: "Red",
-	img: "images/redRoses.jpg",
-	link: "redRose"
+// var redRoses = new app.singleFlower({
+// 	name: "RedRoses",
+// 	price: 39.05,
+// 	color: "Red",
+// 	img: "images/redRoses.jpg",
+// 	link: "redRose"
+// });
+
+// var rainbowRoses = new app.singleFlower({
+// 	name: "RainbowRoses",
+// 	price: 29.05,
+// 	color: "orange",
+// 	link: "rainbowRose"
+// });
+
+// var heirloomRoses = new app.singleFlower({
+// 	name: "heirloomRoses",
+// 	price: 19.05,
+// 	img: "images/heirloomRoses.jpg",
+// 	link: "heirloomRose"
+// });
+
+
+var tantalizingTulips = new app.Flower({
+name: "Tantanlizing Tulips",
+price: 456,
+color: "red"
 });
 
-var rainbowRoses = new app.singleFlower({
-	name: "RainbowRoses",
-	price: 29.05,
-	color: "orange",
-	link: "rainbowRose"
+var fleur = new app.Flower({
+name: "Fleur-De-Lis",
+price: 567,
+color:  "blue"
 });
 
-var heirloomRoses = new app.singleFlower({
-	name: "heirloomRoses",
-	price: 19.05,
-	img: "images/heirloomRoses.jpg",
-	link: "heirloomRose"
-});
-var flowerGroup = new app.FlowersCollection([
-	redRoses,heirloomRoses
-		]);
-flowerGroup.add(rainbowRoses);
+
+var flowerGroup = new app.EuropeanFlower([
+	tantalizingTulips,fleur
+	]);
+tantalizingTulips.set("originCountry","Holland")
 console.log(flowerGroup.toJSON());
 
-//console messages based on instances
-console.log(heirloomRoses.toJSON());
-console.log(rainbowRoses.toJSON());
-console.log(redRoses.toJSON());
+// var flowerGroup = new app.FlowersCollection([
+// 	redRoses,heirloomRoses
+// 		]);
+// flowerGroup.add(rainbowRoses);
+// console.log(flowerGroup.toJSON());
+
+// //console messages based on instances
+// console.log(heirloomRoses.toJSON());
+// console.log(rainbowRoses.toJSON());
+// console.log(redRoses.toJSON());
 
  //price change property
 // rainbowRoses.set('price',20);
--- js/models/singleFlowerModel.js
+++ js/models/singleFlowerModel.js
@@ -1,24 +1,35 @@
+// var app = app || {};
+// app.singleFlower = Backbone.Model.extend ( {
+
+// 	defaults: {
+// 		color: "pink",
+// 		img: "images/sunflower.jpg"
+// 	},
+// 	initialize: function() {
+//  	// 3 instances so 3 times print the message
+//             document.write("Welcome to TutorialsPoint..");
+//             console.log("a mode instance named " + this.get("name") + "has been created and it costs " + this.get("price"));
+
+//         this.on('change', function()
+//                {
+//                	console.log("something in our model has changed")
+//                });
+
+
+//  		this.on('change:price', function()
+//                {
+//                	console.log("the price for the" + this.get("name") + " model just changed to $" + this.get("price") +  " dollars ");
+//                });
+// }
+// })
+
 var app = app || {};
-app.singleFlower = Backbone.Model.extend ( {
-
-	defaults: {
-		color: "pink",
-		img: "images/sunflower.jpg"
-	},
-	initialize: function() {
- 	// 3 instances so 3 times print the message
-            document.write("Welcome to TutorialsPoint..");
-            console.log("a mode instance named " + this.get("name") + "has been created and it costs " + this.get("price"));
-
-        this.on('change', function()
-               {
-               	console.log("something in our model has changed")
-               });
+app.Flower = Backbone.Model.extend( {
 
 
- 		this.on('change:price', function()
-               {
-               	console.log("the price for the" + this.get("name") + " model just changed to $" + this.get("price") +  " dollars ");
-               });
+ initialize: function() {
+   this.on('change',function(){
+            console.log("a mode instance named " + this.get("name") + "has been changed" );
+          });
 }
-})
+});
